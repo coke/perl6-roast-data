dd6ac96f0053f8312fae29101eab88050bf3a364
Will "Coke" Coleda <will@coleda.com>
2012-11-03 20:55:18 -0400

    Need a ./perl6 for test_summary.

--- bin/niecza.sh
+++ bin/niecza.sh
@@ -4,5 +4,9 @@ rm -rf niecza
 git clone git://github.com/sorear/niecza.git
 cd niecza
 ln -s ../../roast t/spec
+echo "#!/usr/bin/env perl" > perl6
+echo 'exec "ulimit -t 15; ulimit -v 1260720; nice -20 mono ./run/Niecza.exe @ARGV"' >> perl6
+chmod a+x ./perl6
 make all
+mono ./run/Niecza.exe -e "say 'BUILD SETTING'"
 /home/coke/perl5/perlbrew/perls/perl-5.14.2/bin/perl t/spec/test_summary niecza 2>&1 | tee ../niecza_summary.out
