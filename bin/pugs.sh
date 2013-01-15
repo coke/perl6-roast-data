4abfa8bb1451c7edcd29b81bba8123fbf25b46ae
Will "Coke" Coleda <will@coleda.com>
2013-01-14 21:05:05 -0500

    fix pug's perl6 wrapper to use right Test.pm

--- bin/pugs.sh
+++ bin/pugs.sh
@@ -17,6 +17,8 @@ cd Pugs.hs
 ln -s ../../roast t/spec
 (cd Pugs && make)
 echo "#!/usr/bin/env perl" > perl6
-echo 'exec "ulimit -t 15; ulimit -v 1260720; nice -20 ./Pugs/pugs @ARGV"' >> perl6
+echo '$ENV{PUGS_USE_EXTERNAL_TEST}=1;' >> perl6
+echo '$ENV{LC_ALL}="en_US.ISO-8859-1";' >> perl6
+echo 'exec "ulimit -t 15; ulimit -v 1260720; nice -20 ./Pugs/pugs -IPugs/ext/Test/lib @ARGV"' >> perl6
 chmod a+x ./perl6
 perl t/spec/test_summary pugs 2>&1 | tee ../pugs_summary.out
