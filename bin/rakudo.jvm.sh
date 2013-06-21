4ad35b0d2114c707561cd0bd6b01a59bbd385d4f
Will "Coke" Coleda <will@coleda.com>
2013-06-21 19:25:38 -0400

    Add a runner for rakudo.jvm

---
+++ bin/rakudo.jvm.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# default to sysperl
+PATH=/usr/local/bin:$PATH
+
+rm -rf rakudo.jvm
+git clone git://github.com/rakudo/rakudo.git rakudo.jvm
+cd rakudo.jvm
+ln -s ../../roast t/spec
+git clone git://github.com/perl6/nqp.git
+cd nqp
+perl ConfigureJVM.pl --prefix=../install-jvm
+make install
+cd ..
+perl ConfigureJVM.pl 
+make all
+
+# uninstalled rakudo doesn't know how to find Test.pm
+# ... or any other modules
+export PERL6LIB=`pwd`/lib:.
+
+# some tests require a LANG.
+export LANG=en_US.UTF-8
+
+# swap out the default runner with one that is ulimited
+mv perl6 p6
+echo "#!/usr/bin/env perl" > perl6
+echo 'exec "ulimit -t 300; ulimit -v 2048576; ./p6 @ARGV";' >> perl6
+chmod u+x perl6 p6
+
+perl t/spec/test_summary rakudo.jvm 2>&1 | tee ../rakudo.jvm_summary.out
