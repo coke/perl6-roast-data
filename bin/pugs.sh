a558235de8597aac92020fdce2135b6fc3ab9dcc
Will "Coke" Coleda <will@coleda.com>
2013-08-29 02:17:28 +0000

    Up the timer from 15s to 1m
    Should avoid some timeout errors on niecza.

--- bin/pugs.sh
+++ bin/pugs.sh
@@ -19,6 +19,6 @@ ln -s ../../roast t/spec
 echo "#!/usr/bin/env perl" > perl6
 echo '$ENV{PUGS_USE_EXTERNAL_TEST}=1;' >> perl6
 echo '$ENV{LC_ALL}="en_US.ISO-8859-1";' >> perl6
-echo 'exec "ulimit -t 15; ulimit -v 1260720; nice -20 ./Pugs/pugs -IPugs/ext/Test/lib @ARGV"' >> perl6
+echo 'exec "ulimit -t 60; ulimit -v 1260720; nice -20 ./Pugs/pugs -IPugs/ext/Test/lib @ARGV"' >> perl6
 chmod a+x ./perl6
 perl t/spec/test_summary pugs 2>&1 | tee ../pugs_summary.out
