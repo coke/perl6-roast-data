99143380c6988e1f0fefd8bd200e5fad7fd71a42
Will "Coke" Coleda <will@coleda.com>
2013-09-07 21:21:01 +0000

    Allow longer run times

--- bin/niecza.sh
+++ bin/niecza.sh
@@ -12,7 +12,7 @@ git clone git://github.com/sorear/niecza.git
 cd niecza
 ln -s ../../roast t/spec
 echo "#!/usr/bin/env perl" > perl6
-echo 'exec "ulimit -t 60; ulimit -v 1260720; nice -20 mono ./run/Niecza.exe @ARGV"' >> perl6
+echo 'exec "ulimit -t 90; ulimit -v 1260720; nice -20 mono ./run/Niecza.exe @ARGV"' >> perl6
 chmod a+x ./perl6
 make all
 
