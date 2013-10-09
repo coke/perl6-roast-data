188539e9590693209f255eb499fc5b4cab532426
Will "Coke" Coleda <will@coleda.com>
2013-10-09 13:17:01 +0000

    Don't use eval server for rakudo.jvm
    Avoids condition where the invocations using the same server would start
    to fail at some point, with occasional information about an OOME.

--- bin/rakudo.jvm.sh
+++ bin/rakudo.jvm.sh
@@ -25,12 +25,4 @@ export LANG=en_US.UTF-8
 # swap out the default runner with one that is ulimited
 # temporarily ignore memory ulimit. ## ulimit -v 2048576
 
-exec 3> >( ./perl6-eval-server -bind-stdin -cookie TESTCOOKIE -app perl6.jar 2>&1 | 
-               tee eval-server.log )
-
-mv perl6 p6
-echo "#!/usr/bin/env perl" > perl6
-echo 'exec "perl eval-client.pl TESTCOOKIE run @ARGV";' >> perl6
-chmod u+x perl6
-
 perl t/spec/test_summary rakudo.jvm 2>&1 | tee ../rakudo.jvm_summary.out
