dd6128cc9bca7d515bef6d73c5f8c1a817a3cace
Will "Coke" Coleda <will@coleda.com>
2013-12-08 23:53:50 +0000

    Start running rakudo-moar...
    ... even though it doesn't build yet.

---
+++ bin/rakudo.moar.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# default to sysperl
+PATH=/usr/local/bin:$PATH
+
+rm -rf rakudo.moar
+git clone git://github.com/rakudo/rakudo.git rakudo.moar
+cd rakudo.moar
+git checkout moar-support
+ln -s ../../roast t/spec
+perl Configure.pl --backends=moar --gen-moar --gen-nqp
+make all
+
+# uninstalled rakudo doesn't know how to find Test.pm
+# ... or any other modules
+export PERL6LIB=`pwd`/lib:.
+
+# some tests require a LANG.
+export LANG=en_US.UTF-8
+
+perl t/spec/test_summary rakudo.moar 2>&1 | tee ../rakudo.moar_summary.out
