effa80add4536260234dd3a902ac90300ed87db0
Will "Coke" Coleda <will@coleda.com>
2014-01-08 19:11:45 +0000

    Allow tests moar time.

--- bin/rakudo.moar.sh
+++ bin/rakudo.moar.sh
@@ -14,7 +14,7 @@ make all
 
 # default build generates a "perl6-m" - need "./perl6" for test_summary
 echo "#!/usr/bin/env perl" > perl6
-echo 'exec "ulimit -t 90; ulimit -v 1260720; ulimit -c 0; nice -20 ./perl6-m @ARGV"' >> perl6
+echo 'exec "ulimit -t 180; ulimit -v 1260720; ulimit -c 0; nice -20 ./perl6-m @ARGV"' >> perl6
 chmod a+x ./perl6
 
 # uninstalled rakudo doesn't know how to find Test.pm
