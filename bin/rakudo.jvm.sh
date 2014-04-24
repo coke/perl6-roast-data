efdeeb48dbd2fa31901cc08b153b02e9f065b872
Will "Coke" Coleda <will@coleda.com>
2014-04-24 19:25:32 +0000

    rakudo.jvm needs more memory & time than others

--- bin/rakudo.jvm.sh
+++ bin/rakudo.jvm.sh
@@ -20,7 +20,7 @@ export LANG=en_US.UTF-8
 
 # swap out the default runner with one that is ulimited
 echo "#!/usr/bin/env perl" > perl6
-echo 'exec "ulimit -t 90; ulimit -v 1260720; ulimit -c 0; nice -20 ./perl6-j @ARGV"' >> perl6
+echo 'exec "ulimit -t 120; ulimit -v 2500000; ulimit -c 0; nice -20 ./perl6-j @ARGV"' >> perl6
 chmod a+x ./perl6
 
 perl t/spec/test_summary rakudo.jvm 2>&1 | tee ../log/rakudo.jvm_summary.out
