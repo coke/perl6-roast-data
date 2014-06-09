0f823d1b8284f221e05beb2d0affaf2ef1696689
Will "Coke" Coleda <will@coleda.com>
2014-06-09 19:32:07 +0000

    Use eval server for jvm.

--- bin/rakudo.jvm.sh
+++ bin/rakudo.jvm.sh
@@ -18,9 +18,13 @@ export PERL6LIB=`pwd`/lib:.
 # some tests require a LANG.
 export LANG=en_US.UTF-8
 
-# swap out the default runner with one that is ulimited
+# swap out the default runner with one that is ulimited and uses evalserver
 echo "#!/usr/bin/env perl" > perl6
-echo 'exec "ulimit -t 120; ulimit -v 2500000; ulimit -c 0; nice -20 ./perl6-j @ARGV"' >> perl6
+echo 'exec "ulimit -t 120; ulimit -v 2500000; ulimit -c 0; nice -20 perl eval-client.pl TESTCOOKIE run @ARGV";' >> perl6
 chmod a+x ./perl6
+
+# start eval server
+exec 3> >( ./perl6-eval-server -bind-stdin -cookie TESTCOOKIE -app perl6.jar 2>&1 | 
+               tee eval-server.log )
 
 perl t/spec/test_summary rakudo.jvm 2>&1 | tee ../log/rakudo.jvm_summary.out
